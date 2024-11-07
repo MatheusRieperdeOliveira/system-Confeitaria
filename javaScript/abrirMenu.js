a0af21299c33553cdf254828cc655b8dfb23283f
Matheus Gustavo Rieper Matos de Oliveira <mrieper87@gmail.com>
2024-11-07 02:12:25 -0300

    commit inicial

---
+++ javaScript/abrirMenu.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    const boxMenu = document.querySelector('#menu').style.display = 'flex'
+}
+
+function fecharMenu() {
+    const boxMenu = document.querySelector('#menu').style.display = 'none'
+    console.log(typeof boxMenu);
+}
